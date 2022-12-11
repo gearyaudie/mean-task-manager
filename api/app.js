362f42e72f551ef5f29f00df544f1a9ab145f6b8
Geary <gearyaudie@gmail.com>
2022-12-11 15:53:20 +0700

    finish part 3

--- api/app.js
+++ api/app.js
@@ -30,13 +30,73 @@ app.post("/lists", (req, res) => {
 });
 
 // Update List
-app.patch("/lists/:id", (req, res) => {});
+app.patch("/lists/:id", (req, res) => {
+  List.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
 
 // Delete List
-app.delete("/lists/:id", (req, res) => {});
+app.delete("/lists/:id", (req, res) => {
+  List.findOneAndRemove({
+    _id: req.params.id,
+  }).then((removedList) => {
+    res.send(removedList).sendStatus(200);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.get("/lists/:listId/tasks", (req, res) => {
+  // We want to return all tasks that belong to a specific list
+  Task.find({
+    _listId: req.params.listId,
+  }).then((tasks) => {
+    res.send(tasks);
+  });
+});
+
+app.get("/lists/:listId/tasks/:taskId", (req, res) => {
+  Task.findOne({
+    _id: req.params.taskId,
+    _listId: req.params.listId,
+  }).then((task) => res.send(task));
+});
+
+// Patch task
+app.patch("/lists/:listId/tasks/:taskId", (req, res) => {
+  Task.findOneAndUpdate(
+    { _id: req.params.taskId, _listId: req.params.listId },
+    { $set: req.body }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+app.post("/lists/:listId/tasks", (req, res) => {
+  let newTask = new Task({
+    title: req.body.title,
+    _listId: req.params.listId,
+  });
+
+  newTask.save().then((task) => {
+    res.send(task);
+  });
+});
+
+// Delete
+app.delete("/lists/:listId/tasks/:taskId", (req, res) => {
+  Task.findOneAndRemove({
+    _id: req.params.taskId,
+    _listId: req.params.listId,
+  }).then((task) => res.send(task));
 });
 
 app.listen(3000, () => console.log("server is listening on port 3000"));
